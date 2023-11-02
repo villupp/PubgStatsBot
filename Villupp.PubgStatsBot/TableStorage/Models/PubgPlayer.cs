d1f7e378478fb002bcb7097868f1b51ae091cb2a
Toppari <oksanen.topias@gmail.com>
2023-11-02 22:44:30 +0200

    Support case insensitive search from cache

--- Villupp.PubgStatsBot/TableStorage/Models/PubgPlayer.cs
+++ Villupp.PubgStatsBot/TableStorage/Models/PubgPlayer.cs
@@ -10,6 +10,7 @@ namespace OfBot.TableStorage.Models
         public DateTimeOffset? Timestamp { get; set; }
         public ETag ETag { get; set; }
         public string Name { get; set; }
+        public string DisplayName { get; set; }
         public string Id { get; set; }
     }
 }
